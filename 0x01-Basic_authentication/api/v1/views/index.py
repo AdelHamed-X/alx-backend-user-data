0a672f75d02aeba2739bd394591ecf1fd8ed87a5
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-06-04 22:12:02 +0300

    Adding Task 2

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -31,3 +31,10 @@ def unauthorized() -> str:
     """ Returns status 401
     """
     return abort(401)
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> str:
+    """ Returns status 403
+    """
+    return abort(403)
